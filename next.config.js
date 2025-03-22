c7bf7ff8cd22410d7d70783ee4ee64de5646715c
Karim <108280260+kaaf7@users.noreply.github.com>
2025-03-22 12:29:53 +0100

    Update and configure next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   },
   trailingSlash: false,
   reactStrictMode: true,
-  i18n: nextI18nextConfig.i18n,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"],
+    localeDetection: false,
+  },
+  fallbackLng: "en",
+  ns: ["common"],
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
 export default nextConfig;
