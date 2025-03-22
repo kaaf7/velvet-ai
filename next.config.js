eb1cf031c7848e3bf01061bc133febb01c0638d5
Karim <108280260+kaaf7@users.noreply.github.com>
2025-03-22 17:04:56 +0100

    Revert next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
-    ignoreDuringBuilds: true,
-  },
-  trailingSlash: false,
-  reactStrictMode: true,
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "de"],
-    localeDetection: false,
-  },
-  fallbackLng: "en",
-  ns: ["common"],
-  reloadOnPrerender: process.env.NODE_ENV === "development",
+
 };
-export default nextConfig;
+
+export default withNextIntl(nextConfig);
