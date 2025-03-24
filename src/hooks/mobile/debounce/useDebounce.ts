96734b91fd05fdd5ad53ce59cb7d691985747af0
Karim <108280260+kaaf7@users.noreply.github.com>
2025-03-24 20:19:01 +0100

    Update useDebounce

--- src/hooks/mobile/debounce/useDebounce.ts
+++ src/hooks/mobile/debounce/useDebounce.ts
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 
-export const useDebounce = <T>(value: T, delay: number) => {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+export const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     const timer = setTimeout(() => setDebouncedValue(value), delay);
