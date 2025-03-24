1ba44245ce75f81c0db005450526e2feed70a137
Karim <108280260+kaaf7@users.noreply.github.com>
2025-03-24 20:20:48 +0100

    Remove HOME_PATH

---
+++ src/common/constants/constants.ts
@@ -0,0 +1,13 @@
+export const HOME_PATH = "/";
+
+export const SIGNIN_PATH = "/sign-in";
+export const SIGNUP_PATH = "/sign-up";
+export const FORGET_PASSWORD_PATH = "/forgot-password";
+
+export const PRODUCTS_ENTRY_PATH = "/products/*";
+export const ALL_PRODUCTS_PATH = "/all";
+export const NEW_PRODUCTS_PATH = "/new";
+export const COLLECTIONS_PATH = "/colleciton";
+export const TRENDING_PATH = "/trending";
+export const BRANDS_PATH = "/brands";
+export const SEASONAL_PATH = "/seasonal";
